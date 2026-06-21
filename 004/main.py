53558e0ed7b8502b96009296e0d167f50ce8c81d
matsuta28 <matsuta0218@gmail.com>
2026-06-21 15:01:43 +0900

    004 答え合わせ

--- 004/main.py
+++ 004/main.py
@@ -1,20 +1,20 @@
 import sys
-import numpy as np
 input = sys.stdin.readline
 
 def main(H, W, A):
-    B = np.zeros((H, W), dtype=int)
-    BW = np.zeros(H, dtype=int)
-    BH = np.zeros(W, dtype=int)
+    B = [[0] * W for _ in range(H)]
+    BW = [0] * H
+    BH = [0] * W
     for i in range(H):
         for j in range(W):
             BW[i] = BW[i] + A[i][j]
             BH[j] = BH[j] + A[i][j]
-    B = [int(BW[i] + BH[j] - A[i][j]) for i in range(H) for j in range(W)]
-    print(B)
+    B = [[BW[i] + BH[j] - A[i][j] for j in range(W)] for i in range(H)]
     
+    for row in B:
+        print(*row)
+
 if __name__ == '__main__':
     H, W = map(int, input().split())
     A = [list(map(int, input().split())) for _ in range(H)]
-    print("input:", A)
     main(H, W, A)
